a571c787eaa93ce1825cbc6b01df5ac3ace42417
Chris Borrill <chris.borrill@gmail.com>
2020-10-26 14:27:04 +1300

    Fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,26 @@ ClickEncoder encoder(A0, A1);
 int16_t current, value;
 
 void timerIsr() {
-  encoder.service();
+    encoder.service();
 }
 
 void enableHid() {
-  Consumer.begin();
-  Serial.println("enableHid");
-  hidEnabled = true;
-  digitalWrite(RXLED, LOW);
+    Consumer.begin();
+    Serial.println("enableHid");
+    hidEnabled = true;
+    digitalWrite(RXLED, LOW);
 }
 
 void disableHid() {
-  Consumer.end();
-  Serial.println("disableHid");
-  hidEnabled = false;
-  digitalWrite(RXLED, HIGH);
+    Consumer.end();
+    Serial.println("disableHid");
+    hidEnabled = false;
+    digitalWrite(RXLED, HIGH);
 }
 
 void toggleMute() {
-  Serial.println("toggleMute");
-  Consumer.write(MEDIA_VOLUME_MUTE);
+    Serial.println("toggleMute");
+    Consumer.write(MEDIA_VOLUME_MUTE);
 }
 
 void handleMute() {
@@ -48,51 +48,50 @@ void handleMute() {
 void handleProgram() {
     program.update();
     if (program.released() && !hidEnabled) {
-      enableHid();
+        enableHid();
     } else if (program.pressed() && hidEnabled) {
-      disableHid();
+        disableHid();
     }
 }
 
 void handleVolume() {
-  current += encoder.getValue();
-
-  auto diff = current - value;
-  if (diff < 0) {
-      Consumer.write(MEDIA_VOL_UP);
-      Serial.print(value);
-      Serial.println(" volDown");
-      value = current;
-  } else if (diff > 0) {
-      Consumer.write(MEDIA_VOL_DOWN);
-      Serial.print(value);
-      Serial.println(" volUp");
-      value = current;
-  }
+    current += encoder.getValue();
+    auto diff = current - value;
+    if (diff < 0) {
+        Consumer.write(MEDIA_VOL_UP);
+        Serial.print(value);
+        Serial.println(" volDown");
+        value = current;
+    } else if (diff > 0) {
+        Consumer.write(MEDIA_VOL_DOWN);
+        Serial.print(value);
+        Serial.println(" volUp");
+        value = current;
+    }
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(RXLED, OUTPUT);
-  digitalWrite(RXLED, LOW);
+    Serial.begin(9600);
+    pinMode(RXLED, OUTPUT);
+    digitalWrite(RXLED, LOW);
 
-  pinMode(BUTTON_MUTE, INPUT);
-  digitalWrite(BUTTON_MUTE, HIGH);
-  mute.attach(BUTTON_MUTE, INPUT_PULLUP);
-  mute.interval(5);
+    pinMode(BUTTON_MUTE, INPUT);
+    digitalWrite(BUTTON_MUTE, HIGH);
+    mute.attach(BUTTON_MUTE, INPUT_PULLUP);
+    mute.interval(5);
 
-  program.attach(SWITCH_PROGRAM, INPUT_PULLUP);
-  program.interval(100);
-  program.setPressedState(LOW);
+    program.attach(SWITCH_PROGRAM, INPUT_PULLUP);
+    program.interval(100);
+    program.setPressedState(LOW);
 
-  Timer1.initialize(1000);
-  Timer1.attachInterrupt(timerIsr);
+    Timer1.initialize(1000);
+    Timer1.attachInterrupt(timerIsr);
 }
 
 void loop() {
-  handleProgram();
-  if (hidEnabled) {
-    handleMute();
-    handleVolume();
-  }
+    handleProgram();
+    if (hidEnabled) {
+        handleMute();
+        handleVolume();
+    }
 }
